e3f24f3b57229d10b6ad66aecb4e3b9818f23f50
Arc Zee <z@arcz.ee>
2020-08-12 14:13:02 +0800

    nth

--- web/themes/court/js/z.js
+++ web/themes/court/js/z.js
@@ -3,7 +3,8 @@ let url = '/court/slider?_format=json';
 xhr.onreadystatechange = function () {
   if(xhr.readyState === XMLHttpRequest.DONE){
     if(xhr.status === 200){
-      initSlider();
+		initSlider();
+		initSwiper();
     }
   }
 };
@@ -17,7 +18,7 @@ function initSlider() {
   let slide = slider.querySelector('.swiper-slide');
 
   let res = xhr.response;
-  console.log(res);
+  //console.log(res);
   for(let i = 0; i < res.length; i++) {
     let nSlide = slide.cloneNode(true);
     let a = nSlide.querySelector('a');
@@ -28,17 +29,15 @@ function initSlider() {
     cap.innerText = res[i].title;
     slider.appendChild(nSlide);
   }
-
   slide.remove();
-  initSwiper();
 }
 
 function initSwiper() {
   var swiper = new Swiper('#slider', {
     loop: true,
-    //autoplay: {
-    //    delay: 5000,
-    //},
+    autoplay: {
+        delay: 5000,
+    },
 
     // If we need pagination
     pagination: {
@@ -53,3 +52,29 @@ function initSwiper() {
     },
   })
 }
+
+let tags = document.getElementsByClassName('tags');
+
+for(let i = 0; i < tags.length; i++){
+  if(tags[i].innerText.indexOf('重要') !== -1){
+    tags[i].previousElementSibling.classList.add('font-weight-bold');
+  }
+}
+
+let date = document.getElementsByClassName('date');
+let now = new Date;
+let badge = document.createElement('span');
+badge.className = "badge badge-danger new-icon";
+badge.innerText = "NEW";
+for(let i = 0; i < date.length; i++){
+  let then = new Date(date[i].innerText.trim());
+  let hours = (now - then) / 3600000;
+  console.log(now);
+  console.log(then);
+  console.log(hours);
+  if(hours < 48){
+    date[i].parentNode.appendChild(badge.cloneNode(true));
+    
+  }
+}
+
