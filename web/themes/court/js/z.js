1bcedbddf305f625892d35d3613b3837430c1f61
Arc Zee <z@arcz.ee>
2020-08-13 11:09:13 +0800

    everythins is fine

--- web/themes/court/js/z.js
+++ web/themes/court/js/z.js
@@ -53,28 +53,34 @@ function initSwiper() {
   })
 }
 
-let tags = document.getElementsByClassName('tags');
+function boldTitle() {
+	let tags = document.getElementsByClassName('tags');
 
-for(let i = 0; i < tags.length; i++){
-  if(tags[i].innerText.indexOf('重要') !== -1){
-    tags[i].previousElementSibling.classList.add('font-weight-bold');
-  }
+	for(let i = 0; i < tags.length; i++){
+		if(tags[i].innerText.indexOf('重要') !== -1){
+			tags[i].previousElementSibling.classList.add('font-weight-bold');
+		}
+	}
 }
 
-let date = document.getElementsByClassName('date');
-let now = new Date;
-let badge = document.createElement('span');
-badge.className = "badge badge-danger new-icon";
-badge.innerText = "NEW";
-for(let i = 0; i < date.length; i++){
-  let then = new Date(date[i].innerText.trim());
-  let hours = (now - then) / 3600000;
-  console.log(now);
-  console.log(then);
-  console.log(hours);
-  if(hours < 48){
-    date[i].parentNode.appendChild(badge.cloneNode(true));
-    
-  }
+function newBadge() {
+	let date = document.getElementsByClassName('date');
+	let now = new Date;
+	let badge = document.createElement('span');
+	badge.className = "badge badge-danger new-icon";
+	badge.innerText = "NEW";
+	for(let i = 0; i < date.length; i++){
+		let then = new Date(date[i].innerText.trim());
+		let hours = (now - then) / 3600000;
+		console.log(now);
+		console.log(then);
+		console.log(hours);
+		console.log(date[i]);
+		if(hours < 48){
+			date[i].parentNode.appendChild(badge.cloneNode(true));
+		}
+	}
 }
 
+boldTitle();
+newBadge();
