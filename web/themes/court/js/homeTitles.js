fec0568f973a559a44f8882c47cbe02a6fc8855e
Arc Zee <z@arcz.ee>
2020-08-13 12:50:35 +0800

    window.onload in homeTitles

--- web/themes/court/js/homeTitles.js
+++ web/themes/court/js/homeTitles.js
@@ -17,4 +17,18 @@ function newBadge() {
 	}
 }
 
-newBadge();
+function boldTitle() {
+	let tags = document.getElementsByClassName('tags');
+
+	for(let i = 0; i < tags.length; i++){
+		if(tags[i].innerText.indexOf('重要') !== -1){
+			tags[i].previousElementSibling.classList.add('font-weight-bold');
+		}
+	}
+}
+
+
+window.onload = function(){
+  boldTitle();
+  newBadge();
+};
